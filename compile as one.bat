3aa2136df5d29fdcf031e2eadd549a641cab0d30
ian <topduckofficial@outlook.com>
2025-06-15 11:24:13 -0500

    add Sounds

---
+++ compile as one.bat
@@ -0,0 +1,1 @@
+multify -c -f  cus_sounds.mf phase_3/audio phase_3.5/audio phase_4 phase_5 phase_5.5 phase_6 phase_7 phase_8 phase_9/audio/sfx phase_10 phase_11 phase_12/audio/sfx phase_12/audio/dial phase_13
