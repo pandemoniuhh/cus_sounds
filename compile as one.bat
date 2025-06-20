e08d24b6ec94b50693c863916d8ea90661b76ce5
ian <topduckofficial@outlook.com>
2025-06-19 22:12:34 -0500

    remove test

--- compile as one.bat
+++ compile as one.bat
@@ -1,3 +1,3 @@
-multify -c -f  cus_sounds_test.mf phase_3/audio phase_3.5/audio phase_4 phase_5 phase_5.5 phase_6 phase_7 phase_8 phase_9/audio/sfx phase_10 phase_11 phase_12/audio/sfx phase_12/audio/dial phase_13
+multify -c -f  cus_sounds.mf phase_3/audio phase_3.5/audio phase_4 phase_5 phase_5.5 phase_6 phase_7 phase_8 phase_9/audio/sfx phase_10 phase_11 phase_12/audio/sfx phase_12/audio/dial phase_13
 
 tree /f > dir.txt
