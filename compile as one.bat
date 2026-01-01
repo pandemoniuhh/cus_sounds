3c98e17d829f53dd694205acf11e9618b4acd3d2
Ian <topduckofficial@outlook.com>
2025-12-31 18:33:59 -0600

    Update compile as one.bat

--- compile as one.bat
+++ compile as one.bat
@@ -1,4 +1,4 @@
-multify -c -f  cus_sounds.mf phase_3/audio phase_3.5/audio phase_4 phase_5 phase_5.5 phase_6 phase_7 phase_8 phase_9/audio/sfx phase_10 phase_11 phase_12/audio/sfx phase_12/audio/dial phase_13
+multify -c -f  cus_sounds.mf phase_3/audio phase_3.5/audio phase_4 phase_5 phase_5.5 phase_6 phase_7 phase_8 phase_9/audio/sfx phase_10/audio/sfx phase_11 phase_12/audio/sfx phase_12/audio/dial
 
 tree /f | more +2 > dir.txt
 
